31216fbbb8965092cb2711409cdbd07ce31033ac
dt1001 <dtang1001@gmail.com>
2020-01-25 22:32:39 -0800

    Inital Commit

---
+++ TreezSimpleOrderService/src/main/java/com/treez/orderservice/TreezSimpleOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.treez.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreezSimpleOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreezSimpleOrderServiceApplication.class, args);
+	}
+
+}
